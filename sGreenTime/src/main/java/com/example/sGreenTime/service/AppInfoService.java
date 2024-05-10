adda10ea30891b9a555e9ba4eae3a46044fb9960
choiyukyung <121738855+choiyukyung@users.noreply.github.com>
2024-05-10 12:06:59 +0900

    Update digital carbon footprints by App

--- sGreenTime/src/main/java/com/example/sGreenTime/service/AppInfoService.java
+++ sGreenTime/src/main/java/com/example/sGreenTime/service/AppInfoService.java
@@ -1,23 +1,16 @@
 package com.example.sGreenTime.service;
 
-import com.example.sGreenTime.dto.MemberDTO;
-import com.example.sGreenTime.dto.UsageStatsDTO;
 import com.example.sGreenTime.entity.AppInfoEntity;
-import com.example.sGreenTime.entity.MemberEntity;
 import com.example.sGreenTime.entity.UsageStatsEntity;
 import com.example.sGreenTime.repository.AppInfoRepository;
 import com.example.sGreenTime.repository.UsageStatsRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -39,48 +32,48 @@ public class AppInfoService {
             //앱 이름 파싱 불가능 문제
             //동영상 어플
             if(appName == "youtube"){
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "netflix") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "tving") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "wavve") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "coupang play") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "disney+") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.46);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             }
             ///
 
             else if (appName == "X") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.55);
+                appCarbon = (float) (Float.valueOf(appTime)*0.52);
             } else if (appName == "linkedIn") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.71);
+                appCarbon = (float) (Float.valueOf(appTime)*0.47);
             } else if (appName == "facebook") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.79);
+                appCarbon = (float) (Float.valueOf(appTime)*0.63);
             }
 
             //채팅 어플
             else if (appName == "snapchat") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.87);
+                appCarbon = (float) (Float.valueOf(appTime)*0.65);
             } else if (appName == "kakaotalk") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.87);
+                appCarbon = (float) (Float.valueOf(appTime)*0.65);
             } else if (appName == "line") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.87);
+                appCarbon = (float) (Float.valueOf(appTime)*0.65);
             } else if (appName == "telegram") {
-                appCarbon = (float) (Float.valueOf(appTime)*0.87);
+                appCarbon = (float) (Float.valueOf(appTime)*0.65);
             }
             ///
 
             else if (appName == "instagram") {
-                appCarbon = (float) (Float.valueOf(appTime)*1.05);
+                appCarbon = (float) (Float.valueOf(appTime)*0.87);
             } else if (appName == "pinterest") {
-                appCarbon = (float) (Float.valueOf(appTime)*1.3);
+                appCarbon = (float) (Float.valueOf(appTime)*0.66);
             } else if (appName == "reddit") {
-                appCarbon = (float) (Float.valueOf(appTime)*2.48);
+                appCarbon = (float) (Float.valueOf(appTime)*0.92);
             } else if (appName == "tiktok") {
-                appCarbon = (float) (Float.valueOf(appTime)*2.63);
+                appCarbon = (float) (Float.valueOf(appTime)*0.96);
             } else {
                 appCarbon = (float) (Float.valueOf(appTime)*0.1);
             }
